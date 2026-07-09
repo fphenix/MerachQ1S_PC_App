f5cc5991290ecb000f60b5e76f0107f256e371f1
Fred L <fphenix@hotmail.com>
2026-07-09 17:16:41 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,10 @@
+DRAG = 2.8
+GUI_REFRESH_MS = 100
+
+WINDOW_TITLE = "Merach PM Monitor"
+WINDOW_WIDTH = 900
+WINDOW_HEIGHT = 700
+
+# BlueTooth address of your Q1S Merach machine. Use a BT scanner to get it.
+# Adresse Bluetooth du rameur Merach Q1S. Utiliser un scanner BT pour l'obtenir
+ROWER_ADDRESS = "24:00:0C:A0:A2:E7"
