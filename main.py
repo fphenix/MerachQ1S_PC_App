d5b0027106eb1cf75159e6fb9ac9a37079e72004
Fred L <fphenix@hotmail.com>
2026-07-17 18:44:11 +0200

    Update main.py with FTMS library modification note
    Added important note about modifying the on_notify method in the PyFTMS library to fix a bug.

--- main.py
+++ main.py
@@ -2,6 +2,41 @@
 main.py
 
 Point d'entrée de l'application Merach PM Monitor.
+
+!!! IMPORTANT NOTE !!!:
+
+It uses pyftms 0-4-15 but there is a bug in C:\Users\<????>\AppData\Local\Programs\Python\Python313\Lib\site-packages\pyftms\client\backends\update.py
+
+I had to modify the on_notify() method in the DataUpdater class of the PyFTMS 0.4.15 lib like this:
+
+def _on_notify(self, c: BleakGATTCharacteristic, data: bytearray) -> None:
+    _LOGGER.debug("Received notify: %s", data.hex(" ").upper())
+    data_ = self._serializer.deserialize(data)._asdict()
+    _LOGGER.debug("Received notify dict: %s", data_)
+    self._result |= data_
+
+    # If `More Data` bit is set - we must wait for other messages.
+    if data[0] & 1:
+        _LOGGER.debug("'More Data' bit is set. Waiting for next data.")
+        return
+
+    # My device sends a lot of null packets during wakeup and sleep mode.
+    # So I just filter null packets.
+    if any(self._result.values()):
+        #correctif par ChatGPT:
+        update = {
+            k: v
+            for k, v in self._result.items()
+            if self._prev.get(k) != v
+        }
+        if update:
+            _LOGGER.debug("Update data: %s", update)
+            update = cast(UpdateEventData, update)
+            self._cb(UpdateEvent(event_id="update", event_data=update))
+            self._prev = self._result.copy()
+            
+    self._result.clear()
+
 """
 
 import sys
