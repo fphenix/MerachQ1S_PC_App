aac7c42867c2df558fcf004e6f3d40d772d24765
Fred L <fphenix@hotmail.com>
2026-09-10 11:17:33 +0200

    Delete snapshot.py
    Moved to engine/

--- snapshot.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from rowers.data import RowerData
-
-# =============================================================================
-@dataclass(slots=True)
-class Snapshot:
-    
-    rowerdata: RowerData
